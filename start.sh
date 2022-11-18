30a13959ec723d53c8fabb056bf91021f6907c77
trey-worsham <117782559+trey-worsham@users.noreply.github.com>
2022-11-18 10:18:59 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+cp zsh-config-/.zshrc .zshrc
